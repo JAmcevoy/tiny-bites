2f4667b04f8295e3f07b5ae16b2a3304495b87d0
JAmcevoy <joshmcevoy18@gmail.com>
2024-05-07 09:01:25 +0000

    Added Pagination to my_bites and changed post layout for the page

--- feed/views.py
+++ feed/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from django.contrib import messages
 from django.utils.text import slugify
+from django.core.paginator import Paginator
 from .models import Create, Review
 from .forms import PostFormCreate, PostFormReview, CommentForm
 
@@ -43,7 +44,7 @@ def post_detail(request, slug):
     )
 
 def post_creation(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         create_form = PostFormCreate(request.POST, request.FILES)
         if create_form.is_valid():
             new_post = create_form.save(commit=False)
@@ -53,19 +54,20 @@ def post_creation(request):
 
             suffix = 1
             while Create.objects.filter(slug=new_post.slug).exists():
-                new_post.slug = slugify(new_post.name) + '-' + str(suffix)
+                new_post.slug = slugify(new_post.name) + "-" + str(suffix)
                 suffix += 1
             
             new_post.save()
 
-            return redirect('post_detail', slug=new_post.slug)
+            return redirect("post_detail", slug=new_post.slug)
     else:
         create_form = PostFormCreate()
 
-    return render(request, 'feed/post_creation.html', {'post_form': create_form})
+    return render(request, "feed/post_creation.html", {"post_form": create_form})
+
 
 # def review_creation(request):
-#     if request.method == 'POST':
+#     if request.method == "POST":
 #         review_form = ReviewForm(request.POST)
 #         if review_form.is_valid():
 #             new_review = review_form.save(commit=False)
@@ -75,26 +77,34 @@ def post_creation(request):
 
 #             suffix = 1
 #             while Review.objects.filter(slug=new_review.slug).exists():
-#                 new_review.slug = slugify(new_review.name) + '-' + str(suffix)
+#                 new_review.slug = slugify(new_review.name) + "-" + str(suffix)
 #                 suffix += 1
             
 #             new_review.save()
 
-#             return redirect('post_detail', slug=new_review.slug)
+#             return redirect("post_detail", slug=new_review.slug)
 #     else:
 #         review_form = ReviewForm()
 
-#     return render(request, 'feed/post_creation.html', {'review_form': review_form})
+#     return render(request, "feed/post_creation.html", {"review_form": review_form})
 
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def my_bites(request):
     if request.user.is_authenticated:
         user_posts = Create.objects.filter(author_id=request.user.id)
-        return render(request, 'feed/my_bites.html', {'user_posts': user_posts})
+        
+        paginator = Paginator(user_posts, 6)
+        page = request.GET.get('page')
+        try:
+            user_posts = paginator.page(page)
+        except PageNotAnInteger:
+            user_posts = paginator.page(1)
+        except EmptyPage:
+            user_posts = paginator.page(paginator.num_pages)
+        
+        return render(request, "feed/my_bites.html", {'user_posts': user_posts})
     else:
-        return render(request, 'account/login.html')
-
-
-
-
+        return render(request, "account/login.html")
 

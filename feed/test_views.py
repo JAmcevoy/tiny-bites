e2be679081a4637218a18504bb7e9803c9541925
JAmcevoy <joshmcevoy18@gmai.com>
2024-05-21 10:02:07 +0000

    Tested search_feature view with a POST request with test_search_feature_post

--- feed/test_views.py
+++ feed/test_views.py
@@ -14,3 +14,18 @@ class PostListViewTest(TestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'feed/index.html')
+
+class SearchFeatureViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('search_feature')
+        self.create1 = Create.objects.create(name='Test Post 1')
+        self.create2 = Create.objects.create(name='Another Test Post')
+
+    def test_search_feature_post(self):
+        response = self.client.post(self.url, {'search_query': 'Test'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'feed/search_results.html')
+        self.assertContains(response, 'Test Post 1')
+        self.assertContains(response, 'Another Test Post')

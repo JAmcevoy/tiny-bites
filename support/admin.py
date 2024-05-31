439aa4c802c2955db29800572a5adf575a450580
JAmcevoy <joshmcevoy18@gmail.com>
2024-05-31 17:14:27 +0000

    Added better filtering and admin fields to error form

--- support/admin.py
+++ support/admin.py
@@ -1,30 +1,13 @@
 from django.contrib import admin
 from .models import ErrorReport
 
-
-class StepsTakenToCorrectFilter(admin.SimpleListFilter):
-    title = 'Steps Taken To Correct'
-    parameter_name = 'steps_taken_to_correct'
-
-    def lookups(self, request, model_admin):
-        return (
-            ('null', 'Steps Not Taken'),
-            ('not_null', 'Steps Taken'),
-        )
-
-    def queryset(self, request, queryset):
-        """
-        Filter queryset based on whether steps have been taken to correct the reported issue.
-        """
-        if self.value() == 'null':
-            return queryset.filter(steps_taken_to_correct__isnull=True)
-        elif self.value() == 'not_null':
-            return queryset.exclude(steps_taken_to_correct__isnull=True)
-
-
 @admin.register(ErrorReport)
 class ErrorReportAdmin(admin.ModelAdmin):
-    list_display = ('issue', 'email', 'description', 'steps_taken_to_correct')
-    search_fields = ['issue']
-    list_filter = (StepsTakenToCorrectFilter,)
-    
+    list_display = ('issue', 'email', 'description', 'status', 'submitted_at', 'handled_by')
+    list_filter = ('status', 'submitted_at')
+    search_fields = ('issue', 'email', 'description')
+
+    def save_model(self, request, obj, form, change):
+        if change: 
+            obj.handled_by = request.user
+        obj.save()
--- support/models.py
+++ support/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class ErrorReport(models.Model):
+    STATUS_CHOICES = [
+        ('open', 'Open'),
+        ('in_progress', 'In Progress'),
+        ('closed', 'Closed'),
+    ]
+
     issue = models.CharField(max_length=100)
     email = models.EmailField()
     description = models.TextField()
     submitted_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='open')
     steps_taken_to_correct = models.TextField(default=None, blank=True, null=True)
+    handled_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, limit_choices_to={'is_superuser': True})
